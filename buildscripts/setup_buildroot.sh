936d5689c8796e8351944c951ecc81a6f32ba939
Tan Do <dmtan90@gmail.com>
2022-01-16 09:56:26 +0700

    Test kernel

--- buildscripts/setup_buildroot.sh
+++ buildscripts/setup_buildroot.sh
@@ -64,5 +64,7 @@ make savedefconfig BR2_DEFCONFIG=/src/config/ingenic_t31_defconfig
 
 # Start the build process
 cd /openmiko/build/buildroot-2016.02
-make
+
+# Fastest build with multiple-cores
+make -j16
 
